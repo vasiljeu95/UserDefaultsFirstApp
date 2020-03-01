703c0a33a67d730a08239c19db4dc99ddfcf4a45
vasiljeu95 <vasilyeu95@gmail.com>
2020-03-02 01:45:42 +0300

    Initial Commit

---
+++ UserDefaultsFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDefaultsFirstApp
+//
+//  Created by Stepan Vasilyeu on 3/2/20.
+//  Copyright © 2020 Stepan Vasilyeu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
